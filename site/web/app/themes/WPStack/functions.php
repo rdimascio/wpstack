244033119c262ba8106f244aa9585390cd7bfadf
Ryan <ryan.dimascio@gmail.com>
2018-01-02 19:58:08 -0800

    acf

--- site/web/app/themes/WPStack/functions.php
+++ site/web/app/themes/WPStack/functions.php
@@ -26,3 +26,17 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+/**
+ * Enable features from Soil when plugin is activated
+ * @link https://roots.io/plugins/soil/
+ */
+add_theme_support('soil-clean-up');
+add_theme_support('soil-disable-asset-versioning');
+add_theme_support('soil-disable-trackbacks');
+add_theme_support('soil-google-analytics', 'UA-XXXXX-Y');
+add_theme_support('soil-jquery-cdn');
+add_theme_support('soil-js-to-footer');
+add_theme_support('soil-nav-walker');
+add_theme_support('soil-nice-search');
+add_theme_support('soil-relative-urls');
