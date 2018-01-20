ecc4987e1d11b48776c568b155b090b36912e457
Ryan <ryan.dimascio@gmail.com>
2018-01-20 09:22:27 -0800

    clean up

--- site/web/app/themes/WPStack/front-page.php
+++ site/web/app/themes/WPStack/front-page.php
@@ -7,7 +7,7 @@
         <header>
             <h1>WordPress plugin stacks, hand-curated by <br />designers & developers.</h1>
             <p>Our vetted team of WordPress ninjas have taken the guesswork out of finding the perfect plugins for your <br />WordPress powered blog, portfolio, store, or website.</p><br />
-            <button class="special"><span class="btn_icon">👇</span> Find a Stack</button> &nbsp; <button href="https://m.me/wpstack"><span class="btn_icon">🙋‍♀️</span> Recommend a Stack</button>
+            <button class="special"><span class="btn_icon">👇</span> Find a Stack</button> &nbsp; <button class="not_so_special" href="https://m.me/wpstack"><span class="btn_icon">🙋‍♀️</span> Recommend a Stack</button>
         </header>
     </section>
     <section id="bundles" class="tiles">
@@ -34,4 +34,19 @@
     <section id="plugins" class="cards">
 
     </section>
+</div>
+<div class="popup">
+    <div class="overlay">
+        <div id="close">
+            <p>🙅‍♀️ Close</p>
+        </div>
+        <div id="chat">
+            <h4>👋 Hey there!</h4>
+            <p>Know a plugin that belongs in one of these stacks?</p>
+            <a href="https://m.me/ryan.dimascio" class="fb-msg-btn" target="_blank"><img src="<?= get_template_directory_uri() . '/dist/images/fb-promoter_messenger.png'; ?>" width="20" class="fb_icon" /> Send to Messenger</a>
+        </div>
+    </div>
+    <div id="ryan">
+        <a href="https://twitter.com/ryandmas/" target="_blank"><img src="<?= get_template_directory_uri() . '/dist/images/ryan.jpg'; ?>" width="50" class="ryan" /></a>
+    </div>
 </div>
